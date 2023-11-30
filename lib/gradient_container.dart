67d1e62770d12ef5490d067a8039faf35c30db1a
Loay Ashraf <loay.ashraf.96@gmail.com>
2023-11-30 17:52:59 +0200

    feat: add dice roller with randomizer
    Resolves: none.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
+import 'package:roll_dice/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -31,8 +17,8 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-          child:
-              StyledText(text: 'Hello World!', color: Colors.white, size: 28,)),
+        child: DiceRoller(),
+      ),
     );
   }
 }
